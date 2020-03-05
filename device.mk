7e424e4cd3e33d387d0f3760b2f366b33bce8379
Fenglin Wu <fenglinw@codeaurora.org>
2020-03-05 19:31:14 +0800

    violet: vibrator: Add config to exclude haptics devices from input subsystem
    Haptics devices which registers as input FF devices are not supposed to
    be detected by input subsystem. Add excluded-input-devices.xml file for
    input subsystem to exclude them.
    
    Change-Id: I52dc6e502b29e2813299d073195a0991bb2cf692
    Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>

--- device.mk
+++ device.mk
@@ -418,6 +418,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
 #VioletParts init
 PRODUCT_PACKAGES += \
     init.violetparts.sh \
