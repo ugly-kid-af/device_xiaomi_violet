ef12a3c6777794ea970ec846594c5af2e553bc24
princejb786 <sam.jbjin@gmail.com>
2020-10-31 09:38:33 +0000

    violet: set BUILD_BROKEN_DUP_RULES= true *Also increase loop to 16
    Signed-off-by: princejb786 <sam.jbjin@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,6 +11,7 @@ DEVICE_PATH := device/xiaomi/violet
 
 # Build
 BUILD_BROKEN_VINTF_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_DUP_RULES := true
 
 # Architecture
 TARGET_ARCH := arm64
@@ -48,7 +49,7 @@ BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 1
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 service_locator.enable=1 swiotlb=1 earlycon=msm_geni_serial,0x880000 loop.max_part=7
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 service_locator.enable=1 swiotlb=1 earlycon=msm_geni_serial,0x880000 loop.max_part=16
 BOARD_KERNEL_CMDLINE +=  androidboot.vbmeta.avb_version=1.0
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
