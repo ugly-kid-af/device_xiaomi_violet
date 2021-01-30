569a33c1638af859e3b4d5e984d03cfb1f8c83a2
ugly-kid-af <dwarmachine24@gmail.com>
2021-01-30 13:27:51 +0530

    violet: bc.mk: add SELINUX_IGNORE_NEVERALLOWS=true
    Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -223,6 +223,7 @@ BOARD_SEPOLICY_M4DEFS += \
     sysfs_ssr=vendor_sysfs_ssr \
     wcnss_service_exec=vendor_wcnss_service_exec \
     wifi_vendor_data_file=vendor_wifi_vendor_data_file
+SELINUX_IGNORE_NEVERALLOWS=true
 
 # WiFi
 BOARD_HOSTAPD_DRIVER := NL80211
