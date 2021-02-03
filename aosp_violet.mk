9fe8b8c221ee75e13088014e382f0bf36c120174
princejb786 <sam.jbjin@gmail.com>
2021-02-03 20:00:31 +0530

    violet: Update build redfin fp to latest feb 2021
    Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>

--- aosp_violet.mk
+++ aosp_violet.mk
@@ -26,7 +26,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="violet-user 9 PKQ1.181203.001 V11.0.8.0.PFHINXM release-keys" \
