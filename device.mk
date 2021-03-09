05004c34e8cf6880e390b0e5b9d27130539b8d0b
princejb786 <sam.jbjin@gmail.com>
2021-03-09 15:03:07 +0530

    Revert "violet: Remove modem assertion and move to in-built firmware"
    This reverts commit 8e04493d070ed09f2bd05da6bc6984b77d4ee7b2.

--- device.mk
+++ device.mk
@@ -211,9 +211,6 @@ PRODUCT_PACKAGES += \
     libqcomfm_jni \
     qcom.fmradio
 
-# Firmware
-$(call inherit-product, vendor/xiaomi-firmware/violet/firmware.mk)
-
 # Framework detect
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor \
@@ -358,6 +355,10 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.sh \
     init.recovery.qcom.rc \
     ueventd.qcom.rc
+
+# Recovery
+PRODUCT_PACKAGES += \
+    librecovery_updater_xiaomi
 
 # RCS
 PRODUCT_PACKAGES += \
